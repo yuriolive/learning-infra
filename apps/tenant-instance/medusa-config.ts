622c023dd2807a49817bc7c222151d02480ade42
Yuri Olive <yuriso1994@gmail.com>
2026-01-22 15:05:29 -0300

    chore: refactor as per review comment

--- apps/tenant-instance/medusa-config.ts
+++ apps/tenant-instance/medusa-config.ts
@@ -2,7 +2,7 @@ import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
-module.exports = defineConfig({
+export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
