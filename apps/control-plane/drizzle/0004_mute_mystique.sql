49b729a195dc8bc22d652a8426f4c08bcb2b8159
Yuri Olive <yuriso1994@gmail.com>
2026-02-03 20:42:24 -0300

    fix: drizzle schema was wrong

--- apps/control-plane/drizzle/0004_mute_mystique.sql
+++ apps/control-plane/drizzle/0004_mute_mystique.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "tenants" ADD COLUMN "jwt_secret" text;--> statement-breakpoint
-ALTER TABLE "tenants" ADD COLUMN "cookie_secret" text;
+ALTER TABLE "tenants" ADD COLUMN "jwt_secret" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "tenants" ADD COLUMN "cookie_secret" text NOT NULL;
