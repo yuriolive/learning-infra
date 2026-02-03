fc662504095c29b138b50ca7cb67fbc758a2e212
Yuri Olive <yuriso1994@gmail.com>
2026-02-03 20:49:08 -0300

    feat: merge migrations into one

--- apps/control-plane/drizzle/0002_long_deathstrike.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "tenants" ADD COLUMN "failure_reason" text;
--- apps/control-plane/drizzle/0004_mute_mystique.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "tenants" ADD COLUMN "jwt_secret" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "tenants" ADD COLUMN "cookie_secret" text NOT NULL;
