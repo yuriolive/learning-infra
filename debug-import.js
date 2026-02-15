0c39fb01ba6cc6c448bb71fe5c2c33fbf8e8d311
yuriolive <yuriso1994@gmail.com>
2026-02-15 20:22:11 -0300

    chore: remove debug file

--- debug-import.js
+++
@@ -1,4 +0,0 @@
-import * as mod from './packages/medusa/plugins/medusa-plugin-neon-search/dist/index.js';
-console.log('Keys:', Object.keys(mod));
-console.log('service:', mod.service);
-console.log('default:', mod.default);
